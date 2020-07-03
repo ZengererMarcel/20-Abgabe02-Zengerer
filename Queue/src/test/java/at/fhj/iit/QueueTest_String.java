66ea8c6d69a6eafad015965560b81637d85079e5
Marcel <marcel.zengerer@edu.fh-joanneum.at>
2020-07-03 17:00:39 +0200

    Add and complete all testcases, reformate Queue

--- Queue/src/test/java/at/fhj/iit/QueueTest_String.java
+++ Queue/src/test/java/at/fhj/iit/QueueTest_String.java
@@ -1,4 +1,79 @@
 package at.fhj.iit;
 
-public class QueueTest_String {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+public class QueueTest_StringTest {
+    Queue queue;
+
+    /**
+     * Init a StringQueue for each test
+     */
+    @BeforeEach
+    void setup() {
+        queue = new Queue(6);
+    }
+
+    @Test
+    @DisplayName("Testing offer Queue")
+    public void testOfferQueue() {
+        assertEquals(queue.offer("Test"), true);
+        assertEquals(queue.offer("Test2"), true);
+        assertEquals(queue.offer("Test3"), true);
+        assertEquals(queue.offer("Test4"), true);
+        assertEquals(queue.offer("Test5"), true);
+        assertEquals(queue.offer("Test6"), true);
+        assertEquals(queue.offer("Test6"), false);
+    }
+
+    @Test
+    @DisplayName("Testing peek Queue")
+    public void testPeekQueue() {
+        assertEquals(queue.peek(), null);
+
+        queue.offer("Test");
+        assertEquals(queue.peek(), "Test");
+
+    }
+
+    @Test
+    @DisplayName("Testing element Queue")
+    public void testElementQueue() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue.element();
+        });
+
+        queue.offer("Test");
+        assertEquals(queue.element(), "Test");
+
+    }
+
+    @Test
+    @DisplayName("Testing poll Queue")
+    public void testPollQueue() {
+
+        assertEquals(queue.poll(), null);
+
+        queue.offer("Test");
+        assertEquals(queue.poll(), "Test");
+
+    }
+
+    @Test
+    @DisplayName("Testing remove Queue")
+    public void testRemoveQueue() {
+        assertThrows(NoSuchElementException.class, () -> {
+            queue.remove();
+        });
+
+        queue.offer("Test");
+        assertEquals(queue.remove(), "Test");
+    }
+
+
 }
